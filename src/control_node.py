98aa94561a4a99510900242e3156aff6491ef1c5
duynamrcv <duynam.robotics@gmail.com>
2024-02-03 14:56:32 +0700

    Finish control node

--- src/control_node.py
+++ src/control_node.py
@@ -3,11 +3,13 @@
 import rospy
 
 from sensor_msgs.msg import LaserScan
-from geometry_msgs.msg import Twist, PoseStamped
+from geometry_msgs.msg import Twist, Pose
 from nav_msgs.msg import Odometry
 from std_msgs.msg import UInt8
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 
 from scheduling.configs import *
+from turtlebot3_scheduling.srv import NextPose, NextPoseResponse
 
 import numpy as np
 import math
@@ -29,19 +31,29 @@ class ControlNode:
 
         # Subscribers
         rospy.Subscriber(rospy.get_namespace()+ODOMETRY_TOPIC, Odometry, self.odomCallback)
-        rospy.Subscriber(rospy.get_namespace()+GOAL_TOPIC, PoseStamped, self.goalCallback)
+
+        # Service Clients
+        rospy.Service(rospy.get_namespace()+NEXTGOAL_TOPIC, NextPose, self.goalCallback)
 
         self.state = State.IDLE
         self.odom = Odometry()
-        self.goal = PoseStamped()
+        self.goal_queue = []
+        self.goal_current = None
 
         self.rate = rospy.Rate(CONTROL_RATE)
 
     def odomCallback(self, data:Odometry):
         self.odom = data
 
-    def goalCallback(self, data:PoseStamped):
-        self.goal = data
+    def goalCallback(self, req):
+        rospy.loginfo("Received new assignment")
+        euler = euler_from_quaternion([req.goal.orientation.x,
+                                       req.goal.orientation.y,
+                                       req.goal.orientation.z,
+                                       req.goal.orientation.w])
+        next_goal = np.array([req.goal.position.x, req.goal.position.y, euler[2]]) # [x, y, yaw]
+        self.goal_queue.append(next_goal)
+        return NextPoseResponse(True)
 
     def publishState(self):
         msg_state = UInt8()
@@ -49,11 +61,62 @@ class ControlNode:
         self.pub_state.publish(msg_state)
 
     def assignmentWaiting(self):
-        if self.goal != PoseStamped():
+        if self.goal_current is None and self.goal_queue != []:
+            self.goal_current = self.goal_queue.pop(0)
+            rospy.loginfo("Moving to next goal: x = {}; y = {}; yaw = {}".format(self.goal_current[0],
+                                                                                 self.goal_current[1],
+                                                                                 self.goal_current[2]))
             self.state = State.ASSIGNED
 
     def assignmentPerforming(self):
-        pass
+        # Check reached goal
+        error = np.hypot(self.odom.pose.pose.position.x - self.goal_current[0],
+                         self.odom.pose.pose.position.y - self.goal_current[1])
+        if error <= EPSILON:
+            self.goal_current = None
+            self.state = State.IDLE
+            self.stopRobot()
+        else:
+            self.computeControlSignal()
+    
+    def stopRobot(self):
+        # Publish control signal
+        vel_msg = Twist()
+        self.pub_vel.publish(vel_msg)
+
+    def computeControlSignal(self):
+        # Get current robot position
+        euler = euler_from_quaternion([self.odom.pose.pose.orientation.x,
+                                       self.odom.pose.pose.orientation.y,
+                                       self.odom.pose.pose.orientation.z,
+                                       self.odom.pose.pose.orientation.w])
+        pose_current = np.array([self.odom.pose.pose.position.x,
+                                self.odom.pose.pose.position.y,
+                                euler[2]])
+        
+        # Control signal
+        x_diff = self.goal_current[0] - pose_current[0]
+        y_diff = self.goal_current[1] - pose_current[1]
+
+        rho = np.hypot(x_diff, y_diff)
+        alpha = (np.arctan2(y_diff, x_diff) - pose_current[2] + np.pi)%(2*np.pi) - np.pi
+        beta = (self.goal_current[2] - pose_current[2] - alpha + np.pi) % (2 * np.pi) - np.pi
+
+        v = KP_RHO*rho
+        w = KP_ALPHA*alpha + KP_BETA*beta
+
+        if alpha > np.pi /2 or alpha < -np.pi/2:
+            v = -v
+        
+        # Normalization
+        v = min(v, MAX_V)
+        w = min(MAX_W, max(w, -MAX_W))
+
+        # Publish control signal
+        vel_msg = Twist()
+        vel_msg.linear.x = v
+        vel_msg.angular.z = w
+        self.pub_vel.publish(vel_msg)
 
     def execute(self):
         while not rospy.is_shutdown():
--- src/scheduling/configs.py
+++ src/scheduling/configs.py
@@ -1,6 +1,15 @@
+import numpy as np
 MODEL = "burger"
 CONTROL_RATE = 20
 EPSILON = 0.05
+
+# Control parameters
+KP_RHO = .9
+KP_ALPHA = 1.5
+KP_BETA = -.3
+
+MAX_V = 1.0
+MAX_W = np.pi/3
 
 # Publish topics
 STATE_TOPIC = "state"           # std_msgs.msg/UInt8
@@ -9,4 +18,6 @@ VEL_TOPIC = "cmd_vel"           # geometry_msgs/Twist
 # Subscribe topics
 ODOMETRY_TOPIC = "odom"         # nav_msgs/Odometry
 LIDAR_TOPIC = "scan"            # sensor_msgs/LaserScan
-GOAL_TOPIC = "goal"             # geometry_msgs/PoseStamped
+
+# Service-Client topics
+NEXTGOAL_TOPIC = "next_goal"    # turtlebot3_scheduling/NextPose
